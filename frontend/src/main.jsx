187b5b376d9863f34bb5832136071dfbc380f227
noliveira895 <n.oliveira895@gmail.com>
2026-05-24 18:01:35 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function App() {
+  return (
+    <div style={{ color: "white", padding: "40px" }}>
+      <h1>FUNCIONANDO</h1>
+    </div>
+  )
+}
